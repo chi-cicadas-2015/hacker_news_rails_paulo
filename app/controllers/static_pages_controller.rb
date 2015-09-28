3980088deeb547671cf3c0387bd3f65e88f64696
pdgonzalez872 <pdgonzalez872@gmail.com>
2015-09-28 17:18:10 -0500

    Finish log in and log out, ready to start posts

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,8 @@
+class StaticPagesController < ApplicationController
+
+  def home
+    # will want to list all the posts here
+  end
+
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
 
+  # This is awesome. REST routes for rails
+  root                'static_pages#home'
+  get    'help'    => 'static_pages#help'
+  get    'about'   => 'static_pages#about'
+  get    'contact' => 'static_pages#contact'
+  get    'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
 
   resources :users
   # get 'users/new'
-  # get 'users/:id'
+  # get 'users/show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
