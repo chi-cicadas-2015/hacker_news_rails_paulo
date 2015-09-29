3fa98c9ef9f599b369007f2f53adc8b500cae964
pdgonzalez872 <pdgonzalez872@gmail.com>
2015-09-28 19:25:14 -0500

    Finish post vertical, will start on comments now. Will eventually change some features in the post section, such as diplay all comments for a certain post. Should be good for now though.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,13 +19,32 @@ class PostsController < ApplicationController
     else
       render 'new'
     end
-
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
 
   def show
     @post = Post.find(params[:id])
     @author = User.find(@post.user_id)
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to root_path
   end
 
 
